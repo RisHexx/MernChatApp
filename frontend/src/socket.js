c936f108615209880a1faffd066437acd5cb8737
Rishexx <privaterishabhh@gmail.com>
2026-03-29 13:32:32 +0530

    done

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://139.59.15.155:5000";
 
-const socket = io(SOCKET_URL, {
+const socket = io("/", {
   //The socket will NOT connect automatically You must manually call: -> socket.connect();
   // so we can control when user connect not directly
   autoConnect: false,
