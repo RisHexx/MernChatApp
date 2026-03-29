b5fb55b129a597934cde830cdcfd1ccf3ccb41b1
Rishexx <privaterishabhh@gmail.com>
2026-03-29 13:10:36 +0530

    Final Deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-  host: true,
-  allowedHosts: [
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // or '0.0.0.0'
+    //By default, Vite only trusts: localhost and 127.0.0.1
+    allowedHosts: [
       'chat.rishabhkanojiya.in'
     ]
+  }
 })
